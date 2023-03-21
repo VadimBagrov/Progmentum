bac207d5a8eacfdde48333a811e74406d350042e
Vadim <vadim.webdev@mail.ru>
2023-03-21 10:46:57 +0300

    update:matrix version 1 ready

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 // Dinamic Matrix background
 
-const s = window.screen;
-const width = q.width = s.width;
-const height = q.height = s.height;
-const letters = Array(256).join(1).split('');
+var s = window.screen;
+var width = q.width = s.width;
+var height = q.height = s.height;
+var letters = Array(256).join(1).split('');
 
-const draw = function () {
+var draw = function () {
     q.getContext('2d').fillStyle="rgba(0,0,0,.05)";
     q.getContext('2d').fillRect(0,0,width,height);
-    q.getContext('2d').fillStyle='#0F0';
-    letters.map(function(y_pos, index){
-        text = String.fromCharCode(65+Math.random()* 33);
-        kpos = index = 10;
-        q.getContext('2d').fillText(text, x_pow, y__pos);
-
-    })
-    setInterval(draw, 33);
-}
+    q.getContext('2d').fillStyle='#C2C2C2';
+    letters.map(function(y_pos, index,){
+        text = String.fromCharCode(65 + Math.random() * 33);
+        x_pos = index * 10;
+        q.getContext('2d').fillText(text, x_pos, y_pos);
+        letters[index] = ( y_pos > 758 + Math.random() * 1e4) ? 0 : y_pos + 10;
+    });
+};
+setInterval(draw , 33);
