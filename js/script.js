b6932444c07bb27771eeece8ad2b28319178e2a6
Vadim <vadim.webdev@mail.ru>
2023-03-23 00:02:51 +0300

    update:fitcher time(and date)  ready

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // Dinamic Matrix background
 
-var s = window.screen;
-var width = q.width = s.width;
-var height = q.height = s.height;
-var letters = Array(256).join(1).split('');
+const s = window.screen;
+const width = q.width = s.width;
+const height = q.height = s.height;
+const letters = Array(256).join(1).split('');
 
-var draw = function () {
+const draw = function () {
     q.getContext('2d').fillStyle="rgba(0,0,0,.05)";
     q.getContext('2d').fillRect(0,0,width,height);
     q.getContext('2d').fillStyle='#C2C2C2';
@@ -16,4 +16,23 @@ var draw = function () {
         letters[index] = ( y_pos > 758 + Math.random() * 1e4) ? 0 : y_pos + 10;
     });
 };
-setInterval(draw , 33);
+setInterval(draw , 43);
+
+// Date and time
+
+setInterval(function() {
+    const time = document.querySelector('.time') //Получаем time из html
+    const date = new Date(); //Получаем текущую дату и время
+    const currentTime = date.toLocaleTimeString(); //Получаем только время
+
+    time.textContent = currentTime;
+}, 1000) // Время
+
+setInterval(function() {
+    const dateNow = document.querySelector('.date')  //Получаем date из html
+    date = new Date(); //Получаем текущую дату и время
+    const options = { weekday: 'long', month: 'long', day: 'numeric' }; //опции для даты
+    const currentDate = date.toLocaleDateString('en-Us', options); //текущая дата
+
+    dateNow.textContent = currentDate;
+}, 1000) //Дата и день недели
